4b18ad0133feccefcb3647621cf3a6c9d8dbb619
bcochon <brunoxosec@gmail.com>
2025-04-08 01:32:29 -0300

    Completo tests

--- src/main/java/domain/Inscripcion.java
+++ src/main/java/domain/Inscripcion.java
@@ -1,14 +1,17 @@
 package domain;
 
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Inscripcion {
-    private List<Materia> materias;
+    private List<Materia> materias = new LinkedList<Materia>();
     private Alumno alumno;
 
-    public Inscripcion(List<Materia> materias, Alumno alumno) {
-        this.materias = materias;
+    public Inscripcion(Alumno alumno, Materia materia, Materia ... materias) {
         this.alumno = alumno;
+        this.materias.add(materia);
+        Collections.addAll(this.materias, materias);
     }
 
     public Boolean aprobada() {
---
+++ src/test/java/domain/InscripcionTest.java
@@ -0,0 +1,45 @@
+package domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class InscripcionTest {
+    Materia analisisMatematico = new Materia();
+    Materia analisisMatematico2 = new Materia(analisisMatematico);
+    Materia algebra = new Materia();
+    Materia analisisNumerico = new Materia(analisisMatematico2, algebra);
+
+    Alumno bruno = new Alumno(analisisMatematico);
+    Alumno juan = new Alumno(algebra);
+    Alumno dante = new Alumno(analisisNumerico, analisisMatematico2, algebra);
+    Alumno pepe = new Alumno();
+
+    @Test
+    @DisplayName("Inscripción aprobada simple")
+    public void revisarInscripcionBuena() {
+        Inscripcion inscripcionBuena = new Inscripcion(dante, analisisNumerico);
+        Assertions.assertEquals(true, inscripcionBuena.aprobada());
+    }
+
+    @Test
+    @DisplayName("Inscripción aprobada compleja")
+    public void revisarInscripcionBuenaMultiple() {
+        Inscripcion inscripcionBuena = new Inscripcion(bruno, analisisMatematico2);
+        Assertions.assertEquals(true, inscripcionBuena.aprobada());
+    }
+
+    @Test
+    @DisplayName("Inscripción desaprobada")
+    public void revisarInscripcionMala() {
+        Inscripcion inscripcionMala = new Inscripcion(pepe, analisisMatematico2);
+        Assertions.assertEquals(false, inscripcionMala.aprobada());
+    }
+
+    @Test
+    @DisplayName("Inscripción desaprobada compleja")
+    public void revisarInscripcionMalaMultiple() {
+        Inscripcion inscripcionMala = new Inscripcion(juan, analisisNumerico);
+        Assertions.assertEquals(false, inscripcionMala.aprobada());
+    }
+}
